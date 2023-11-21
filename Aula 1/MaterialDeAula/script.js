2bdd51ddac8043840fdf4642e43782f9c354cf12
pablokadafi <pablokadafi@hotmail.com>
2023-11-21 17:23:24 -0300

    Aula 1 Js
    JavaScript

---
+++ Aula 1/MaterialDeAula/script.js
@@ -0,0 +1,46 @@
+console.log("Olá mundo:)");
+
+
+
+
+//caixas de texto
+alert("olá, este é o meu primeiro comando Js");
+confirm("Estou aprendendo Js?");
+prompt("Qual é o seu nome?");
+
+//variaveis
+//let escopo local, sobrescrevivel
+function testeLet(){
+    let numero = 0;
+    console.log(0);
+    Number = 10;
+    console.log(numero);
+    prompt
+}
+
+//const escopo local, sobrescrevivel
+const pi = 3.14;
+console.log(pi);
+
+//var escopo global, sobrescrevivel
+var nome = "Pablo";
+console.log(nome);
+
+var idade = prompt("Qual é a sua idade");
+alert("Seja bem vindo!");
+//contatenação
+//tipos de valores
+//number numeros
+var real = 180;
+
+// string textos
+var texto = "exemplo";
+
+//boolean logicos ou verdadeiro e falso
+var ligado = true;
+
+
+//objeto
+var carro = [];
+
+
---
+++ Aula 1/MaterialDeAula/script2.js
@@ -0,0 +1,23 @@
+alert("calculadora de soma");
+var numero1 = number(prompt("Digite o seu primeiro número"));
+var numero2 = number(prompt("Digite o seu segundo número"));
+
+
+
+//primeira maneira
+//var soma = parseFloat(numero1) + parsefloat(numero2);
+
+
+var soma = nunmero1 + numero2;
+
+
+alert(soma);
+
+
+//operadores arimetricos
+// + somar(concatenar)
+// - subtrair
+// * multiplicar
+// ** potencia 
+// / dividir 
+// % resto da divisão
