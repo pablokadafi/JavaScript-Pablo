90066683881e874ee0e2907379cd01647300abc6
pablokadafi <pablokadafi@hotmail.com>
2023-11-22 17:41:01 -0300

    Update script3.js

--- Aula 2-condicional/Material de aula/script3.js
+++ Aula 2-condicional/Material de aula/script3.js
@@ -1,23 +1,23 @@
 var idade = (prompt("Qual é a sua idade?"));
 
- if(idade <= 15){
+ if(idade < 15){
      alert("Criança")
     }
    
- if(idade >= 15 <=30){
+ if(idade >= 15 && idade < 30){
      alert("Jovem")
     }
-    var idade = (prompt("Qual é a sua idade?"));
 
-    if(idade >= 30 <=60){
+
+    if(idade >= 30 && idade < 60){
         alert("Adulto")
        }
-       var idade = (prompt("Qual é a sua idade?"));
+
 
        if(idade >=60){
         alert("Idoso")
        }
-       var idade = (prompt("Qual é a sua idade?"));
+
 
 
 
